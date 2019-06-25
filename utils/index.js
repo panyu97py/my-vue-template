dfaacfb0f86bd41512b9268ca5621a72d5abf84e
yu.pan <yu.pan@hzjo.com>
2019-06-25 11:37:12 +0800

    modify console

--- utils/index.js
+++ utils/index.js
@@ -78,7 +78,7 @@ To get started:
     )}${lintMsg(data)}npm run dev`
   )}
   
-Documentation can be found at https://vuejs-templates.github.io/webpack
+Documentation can be found at https://github.com/panyu97py/my-vue-template
 `
   console.log(message)
 }
