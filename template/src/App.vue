9046ef08fd96b64c6477083e7c45f6dd13a00075
yu.pan <yu.pan@hzjo.com>
2019-06-25 15:54:06 +0800

    modify main.js

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,46 +1,41 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.jpg"
-         class="logo"{{#vuex}}@click="setLogoClickTimes"
-         {{/vuex}}>
+    <img src="./assets/logo.jpg" class="logo" {{#vuex}}@click="setLogoClickTimes" {{/vuex}}>
     <svg-icon icon-class="alarm" />
-     {{#router}}
-    <router-view/>
+    {{#router}}
+    <router-view />
     {{else}}
-    <demo/>
+    <demo />
     {{/router}}
   </div>
 </template>
 
 <script>
-{{#unless router}}
-  import demo from './views/demo'
-{{/unless}}
-{{#vuex}}
-import {mapActions} from 'vuex'
-{{/vuex}}
+{{#unless router}}import demo from './views/demo'{{/unless}}
+{{#vuex}}import {mapActions} from 'vuex'{{/vuex}}
 export default {
   name: 'App'{{#router}}{{else}},
-    components: {
-      demo
-    }{{/router}}{{#vuex}},
-    methods:{
-      ...mapActions(['setLogoClickTimes'])
-    }{{/vuex}}
+  components: {
+    demo
+  }{{/router}}{{#vuex}},
+  methods:{
+    ...mapActions(['setLogoClickTimes'])    
+  }{{/vuex}}
 }
 </script>
 
 <style lang='scss'>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: $globalFontColor;
-  margin-top: 60px;
-  .logo {
-    width: 200px;
-    height: 200px;
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: $globalFontColor;
+    margin-top: 60px;
+
+    .logo {
+      width: 200px;
+      height: 200px;
+    }
   }
-}
 </style>
